651ce6ec081e5c9f907d7ab1f17f41f93f655ee6
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-12-17 22:45:07 -0500

    Get websockets working.

--- src/piper_app.erl
+++ src/piper_app.erl
@@ -8,25 +8,34 @@
 -behaviour(application).
 
 %% API.
--export([start/2]).
--export([stop/1]).
+-export([start/2, stop/1]).
 
 %% API.
 start(_Type, _Args) ->
+    Listener = http,
     Port = 8080,
+    NumberOfAcceptors = 100,
     Dispatch = [
         {'_', [
+            {[<<"websockets">>], piper_websockets_handler, []},
             {['...'], cowboy_static, [
                 {directory, {priv_dir, piper, []}},
                 {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
             ]}
         ]}
     ],
-    {ok, _} = cowboy:start_http(http, 100, [{port, Port}], [
-        {dispatch, Dispatch}
-    ]),
-    lager:info("Application listening on port ~p", [Port]),
-    piper_sup:start_link().
+    Status = cowboy:start_http(Listener,
+                               NumberOfAcceptors,
+                               [{port, Port}],
+                               [{dispatch, Dispatch}]),
+    case Status of
+        {error, _} ->
+            lager:info("Application failed to start on port ~p", [Port]),
+            ok;
+        {ok, _Pid} ->
+            lager:info("Application listening on port ~p", [Port]),
+            piper_sup:start_link()
+    end.
 
 stop(_State) ->
     ok.
---
+++ src/piper_websockets_handler.erl
@@ -0,0 +1,34 @@
+%% @author Christopher Meiklejohn <cmeiklejohn@basho.com>
+%% @copyright 2012 Christopher Meiklejohn
+%% @doc Piper application.
+
+-module(piper_websockets_handler).
+-author('Christopher Meiklejohn <cmeiklejohn@basho.com>').
+
+-export([init/3]).
+
+-export([websocket_init/3,
+         websocket_handle/3,
+         websocket_info/3,
+         websocket_terminate/3]).
+
+init({tcp, http}, _Req, _Opts) ->
+    {upgrade, protocol, cowboy_websocket}.
+
+websocket_init(_TransportName, Req, _Opts) ->
+    erlang:start_timer(1000, self(), <<"Hello!">>),
+    {ok, Req, undefined_state}.
+
+websocket_handle({text, Msg}, Req, State) ->
+    {reply, {text, << "That's what she said! ", Msg/binary >>}, Req, State};
+websocket_handle(_Data, Req, State) ->
+    {ok, Req, State}.
+
+websocket_info({timeout, _Ref, Msg}, Req, State) ->
+    erlang:start_timer(1000, self(), <<"How' you doin'?">>),
+    {reply, {text, Msg}, Req, State};
+websocket_info(_Info, Req, State) ->
+    {ok, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
+    ok.
